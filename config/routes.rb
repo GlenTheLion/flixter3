70647053a72c2c4d1d8be86705fcdc7266d3b461
Glen Datugan <Glen.datugan@gmail.com>
2019-10-02 18:09:17 +0000

    added sections java script.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
 
     resources :lessons, only: [:update]
 
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
 
       resources :lessons, only: [:new, :create]
 
@@ -26,3 +27,25 @@ Rails.application.routes.draw do
   end
   
 end
+
+# Rails.application.routes.draw do
+#   devise_for :users
+#   resource :dashboard, only: [:show]
+#   root 'static_pages#index'
+#   get 'privacy', to: 'static_pages#privacy'
+#   resources :courses, only: [:index, :show] do
+#     resources :enrollments, only: :create
+#   end
+
+#   resources :lessons, only: [:show]
+#   namespace :instructor do
+#     resources :lessons, only: [:update]
+#     resources :sections, only: [:update] do
+#       resources :lessons, only: [:create]
+#     end
+
+#     resources :courses, only: [:new, :create, :show] do
+#       resources :sections, only: [:create]
+#     end
+#   end
+# end
